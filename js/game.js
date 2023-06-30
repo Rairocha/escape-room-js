fb379fa340171927b0bd1f3bb38a988a79be8423
Rairocha <52248306+Rairocha@users.noreply.github.com>
2023-06-30 11:07:29 -0300

    remove script file

--- js/game.js
+++ js/game.js
@@ -84,3 +84,20 @@ play(){
     };
   
 };
+function start(){
+  let game = new Game()
+  game.start();
+  return
+};
+window.onload = function () {
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+  
+
+  startButton.addEventListener("click", function () {
+    start();
+  })
+  restartButton.addEventListener("click", function () {
+      // Call the restartGame function when the button is clicked
+      window.location.reload();
+    });};
